be8f65a328b31d637f0d86c94357b5c1a2de7462
Eduard Manea <Eduard.Manea@softwire.com>
2020-07-02 14:07:24 +0100

    Finished stretch exercise 1

--- marsappspa/src/App.tsx
+++ marsappspa/src/App.tsx
@@ -3,13 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import NasaComponent from './NasaComponent';
 import ClickCountComponent from "./ClickCountComponent";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Component1 from "./Component1";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <Component1 />
+          <Router>
+            <Switch>
+                <Route path = '/nasa'>
+                    <NasaComponent/>
+                    
+                </Route>
+                <Route path = '/counter'>
+                    <Component1 />
+
+                </Route>
+
+            </Switch>
+          </Router>
       </header>
     </div>
   );
